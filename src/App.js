647aebb985daf4f1e0ce3eb65c23ebc8b4120497
jisoooo17 <jisoooo17@gmail.com>
2024-07-19 18:53:02 +0900

    컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -47,8 +47,20 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
 
+      <Modal/>
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세 내용</p>
+    </div>
+  )
 }
 
 export default App;
